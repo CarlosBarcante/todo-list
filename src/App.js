c34e7a4cc074b5a153664d49f6128eb0bf161504
Carlos Barçante <barcantecarlosdev@gmail.com>
2022-06-09 08:59:08 -0300

    primeira versão

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import List from "./components/List";
+import Item from './components/Item';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([]);
+
+    function onAddItem(text) {
+        let item = new Item(text);
+        setItems([...items, item])
+    }
+
+    function onItemDeleted(item) {
+        let filteredItems = items.filter(it => it.id !== item.id);
+        setItems(filteredItems);
+    }
+
+    function onDone(item) {
+        let updatedItems = items.map(it => {
+            if (it.id === item.id) {
+                it.done = !it.done;
+            }
+            return it;
+        })
+        setItems(updatedItems);
+    }
+
+    return (
+        <div className="container">
+            <h1>Todo</h1>
+            <TodoForm onAddItem={onAddItem}></TodoForm>
+            <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
+        </div>
+    )
+
 }
 
 export default App;
